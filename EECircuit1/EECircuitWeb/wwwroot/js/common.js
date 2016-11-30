edb6bf7a1e364599b4172927e895b89d50655e49
Kawamata Akira AK. <autumn@piedey.co.jp>
2016-11-30 17:48:38 +0900

    fill combo

--- EECircuit1/EECircuitWeb/wwwroot/js/common.js
+++ EECircuit1/EECircuitWeb/wwwroot/js/common.js
@@ -10,6 +10,41 @@ function logicalValue()
     }
 }
 
+var logics =
+   [
+       {
+           name: "NOT",
+           table: [[1, 0]]
+       },
+       {
+           name: "AND",
+           table: [[0, 0, 0, 1]]
+       },
+       {
+           name: "OR",
+           table: [[0, 1, 1, 1]]
+       },
+       {
+           name: "XOR",
+           table: [[0, 1, 1, 1]]
+       }
+   ];
+
+function loaded() {
+    for (var i = 0; i < logics.length; i++) {
+        var sel = document.getElementById("typeSelector");
+        var newOpt = new Option();
+        newOpt.text = logics[i].name;
+        sel.appendChild(newOpt);
+    }
+}
+
+
+function typeSelector_Changed()
+{
+
+}
+
 var t = new logicalValue();
 t.value = "L";
 alert(t.toString());
