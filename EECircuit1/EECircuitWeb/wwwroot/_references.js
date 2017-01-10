ebeaf03f09708c213365aa4f3e0f1492e4c349d7
Kawamata Akira AK. <autumn@piedey.co.jp>
2017-01-10 18:07:12 +0900

    suppot and operator

--- EECircuit1/EECircuitWeb/wwwroot/_references.js
+++ EECircuit1/EECircuitWeb/wwwroot/_references.js
@@ -1,5 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/common.js" />
+/// <reference path="js/gate.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
---
+++ EECircuit1/EECircuitWeb/wwwroot/js/gate.js
@@ -0,0 +1,13 @@
+﻿
+function and(a, b) {
+    return a && b;
+}
+
+function update() {
+    var r = and($("#checka").prop('checked'), $("#checkb").prop('checked'));
+    $("#checkr").prop('checked', r);
+    $("#checkr").checkboxradio("refresh");
+}
+
+$("#checka").click(update);
+$("#checkb").click(update);
