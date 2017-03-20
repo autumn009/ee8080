e58b0af5c3fb63a3cb57811c0eed1fa9bc1503e5
Kawamata Akira AK <autumn@piedey.co.jp>
2017-03-20 17:50:56 +0900

    change table creation

--- EECircuit1/EECircuitWeb2/Scripts/logic.js
+++ EECircuit1/EECircuitWeb2/Scripts/logic.js
@@ -96,21 +96,55 @@ function setupOutputFlags(outputLabels) {
     }
 }
 var thefunc;
-function setup(name, pictureName, func, inputLabels, outputLabels) {
-    if (!inputLabels)
-        inputLabels = ["A", "B"];
-    if (!outputLabels)
-        outputLabels = ["Q"];
-    thefunc = func;
+var LogicTableUnit = (function () {
+    function LogicTableUnit() {
+        this.outputValues = [];
+    }
+    return LogicTableUnit;
+}());
+var LogicTable = (function () {
+    function LogicTable() {
+        this.logicTables = [];
+    }
+    return LogicTable;
+}());
+function createLogicalTable(ignoreableInputLabels, inputLabels, outputLabels) {
+    var table = new LogicTable();
+    for (var i = 0; i < Math.pow(2, ignoreableInputLabels.length); i++) {
+        var unit = new LogicTableUnit();
+        var inputValues = [];
+        var t = i;
+        for (var j = 0; j < inputLabels.length; j++) {
+            inputValues.push(((t & 1) != 0));
+            t = t >> 1;
+        }
+        for (var j = 0; j < Math.pow(2, inputLabels.length); j++) {
+            unit.outputValues.push(thefunc(inputValues));
+        }
+        table.logicTables.push(unit);
+    }
+    return table;
+}
+function optimizeTable(table) {
+}
+function createLabel(label) {
+    var thi = document.createElement("th");
+    if (label.substring(0, 1) == "_") {
+        var span = document.createElement("span");
+        $(span).css("text-decoration", "overline");
+        $(span).text(label.substring(1));
+        $(thi).append(span);
+    }
+    else {
+        $(thi).text(label);
+    }
+    $(thi).addClass("borderh");
+    return thi;
+}
+function createHtmlTable(logicTable, ignoreableInputLabels, inputLabels, outputLabels) {
     var expand = false;
     if (inputLabels.length + outputLabels.length <= 3)
         expand = true;
-    $("#logicname").text(name);
-    $("#logicicon").attr("src", "/Content/images/gate/" + pictureName + ".png");
-    $("#simname").text(name + "・シミュレーター");
-    $("#tablename").text(name + "真理表");
-    setupInputChecks(inputLabels);
-    setupOutputFlags(outputLabels);
     $("#tableroot").empty();
     var table = document.createElement("table");
     $("#tableroot").append(table);
@@ -136,39 +170,40 @@ function setup(name, pictureName, func, inputLabels, outputLabels) {
     $(tdInput).append(tableInput);
     var trih = document.createElement("tr");
     $(tableInput).append(trih);
-    for (var i = 0; i < inputLabels.length; i++) {
-        var thi = document.createElement("th");
-        if (inputLabels[i].substring(0, 1) == "_") {
-            var span = document.createElement("span");
-            $(span).css("text-decoration", "overline");
-            $(span).text(inputLabels[i].substring(1));
-            $(thi).append(span);
-        }
-        else {
-            $(thi).text(inputLabels[i]);
-        }
-        $(thi).addClass("borderh");
-        $(trih).append(thi);
+    for (var i = 0; i < ignoreableInputLabels.length; i++) {
+        $(trih).append(createLabel(ignoreableInputLabels[i]));
     }
-    for (var j = 0; j < totalCount; j++) {
-        var trid = document.createElement("tr");
-        $(trid).addClass("tr" + j);
-        $(trid).addClass("trall");
-        $(tableInput).append(trid);
-        var values = [];
-        var t = j;
-        for (var i = 0; i < inputLabels.length; i++) {
-            values.push(((t & 1) != 0));
-            t = t >> 1;
-        }
-        for (var i = 0; i < inputLabels.length; i++) {
-            var thd = document.createElement("td");
-            $(thd).addClass("border");
-            if (expand)
-                $(thd).addClass("thick");
-            $(thd).addClass(values[i] ? "one" : "zero");
-            $(thd).text(values[i] ? "1" : "0");
-            $(trid).append(thd);
+    for (var i = 0; i < inputLabels.length; i++) {
+        $(trih).append(createLabel(inputLabels[i]));
+    }
+    var count = 0;
+    for (var j = 0; j < logicTable.logicTables.length; j++) {
+        for (var k = 0; k < logicTable.logicTables[j].outputValues.length; k++) {
+            var trid = document.createElement("tr");
+            $(trid).addClass("tr" + count);
+            $(trid).addClass("trall");
+            $(tableInput).append(trid);
+            var values = [];
+            var t = k;
+            for (var i = 0; i < inputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            var t = j;
+            for (var i = 0; i < ignoreableInputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            for (var i = 0; i < values.length; i++) {
+                var thd = document.createElement("td");
+                $(thd).addClass("border");
+                if (expand)
+                    $(thd).addClass("thick");
+                $(thd).addClass(values[i] ? "one" : "zero");
+                $(thd).text(values[i] ? "1" : "0");
+                $(trid).append(thd);
+            }
+            count++;
         }
     }
     // create output table
@@ -191,45 +226,72 @@ function setup(name, pictureName, func, inputLabels, outputLabels) {
         $(tho).addClass("result");
         $(troh).append(tho);
     }
-    for (var j = 0; j < totalCount; j++) {
-        var trod = document.createElement("tr");
-        $(trod).addClass("tr" + j);
-        $(trod).addClass("trall");
-        $(tableOutput).append(trod);
-        var values = [];
-        var t = j;
-        for (var i = 0; i < inputLabels.length; i++) {
-            values.push(((t & 1) != 0));
-            t = t >> 1;
-        }
-        var results = thefunc(values);
-        for (var i = 0; i < outputLabels.length; i++) {
-            var thd = document.createElement("td");
-            $(thd).addClass("border");
-            if (expand)
-                $(thd).addClass("thick");
-            $(thd).addClass("result");
-            $(thd).addClass(results[i] ? "one" : "zero");
-            $(thd).text(results[i] ? "1" : "0");
-            $(trod).append(thd);
+    var count = 0;
+    for (var j = 0; j < logicTable.logicTables.length; j++) {
+        for (var k = 0; k < logicTable.logicTables[j].outputValues.length; k++) {
+            var trod = document.createElement("tr");
+            $(trod).addClass("tr" + count);
+            $(trod).addClass("trall");
+            $(tableOutput).append(trod);
+            var values = [];
+            var t = k;
+            for (var i = 0; i < inputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            var t = j;
+            for (var i = 0; i < ignoreableInputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            var results = thefunc(values);
+            for (var i = 0; i < outputLabels.length; i++) {
+                var thd = document.createElement("td");
+                $(thd).addClass("border");
+                if (expand)
+                    $(thd).addClass("thick");
+                $(thd).addClass("result");
+                $(thd).addClass(results[i] ? "one" : "zero");
+                $(thd).text(results[i] ? "1" : "0");
+                $(trod).append(thd);
+            }
+            count++;
         }
     }
+}
+function setup(name, pictureName, func, ignoreableInputLabels, inputLabels, outputLabels) {
+    if (!ignoreableInputLabels)
+        ignoreableInputLabels = [];
+    if (!inputLabels)
+        inputLabels = ["A", "B"];
+    if (!outputLabels)
+        outputLabels = ["Q"];
+    thefunc = func;
+    $("#logicname").text(name);
+    $("#logicicon").attr("src", "/Content/images/gate/" + pictureName + ".png");
+    $("#simname").text(name + "・シミュレーター");
+    $("#tablename").text(name + "真理表");
+    setupInputChecks(ignoreableInputLabels.concat(inputLabels));
+    setupOutputFlags(outputLabels);
+    var table = createLogicalTable(ignoreableInputLabels, inputLabels, outputLabels);
+    optimizeTable(table);
+    createHtmlTable(table, ignoreableInputLabels, inputLabels, outputLabels);
     update();
 }
 function andsetup() {
     setup("AND GATE", "AND", function (input) {
         return [input[0] && input[1]];
-    }, null, null);
+    }, null, null, null);
 }
 $("#navnot").click(function () {
     setup("NOT GATE", "NOT", function (input) {
         return [!input[0]];
-    }, ["A"], null);
+    }, null, ["A"], null);
 });
 $("#navor").click(function () {
     setup("OR GATE", "OR", function (input) {
         return [input[0] || input[1]];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navand").click(function () {
     andsetup();
@@ -237,22 +299,22 @@ $("#navand").click(function () {
 $("#navand4").click(function () {
     setup("AND(4Input) GATE", "AND(4Input)", function (input) {
         return [input[0] && input[1] && input[2] && input[3]];
-    }, ["A", "B", "C", "D"], null);
+    }, null, ["A", "B", "C", "D"], null);
 });
 $("#navxor").click(function () {
     setup("XOR GATE", "XOR", function (input) {
         return [input[0] !== input[1]];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navnor").click(function () {
     setup("NOR GATE", "NOR", function (input) {
         return [!(input[0] || input[1])];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navnand").click(function () {
     setup("NAND GATE", "NAND", function (input) {
         return [!(input[0] && input[1])];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navdec").click(function () {
     setup("DECODER", "DECODER", function (input) {
@@ -260,7 +322,7 @@ $("#navdec").click(function () {
         if (input[3] && !input[4] && !input[5])
             n = (input[0] ? 1 : 0) + (input[1] ? 2 : 0) + (input[2] ? 4 : 0);
         return [n != 0, n != 1, n != 2, n != 3, n != 4, n != 5, n != 6, n != 7];
-    }, ["A", "B", "C", "G1", "_G2A", "_G2B"], ["_Y0", "_Y1", "_Y2", "_Y3", "_Y4", "_Y5", "_Y6", "_Y7"]);
+    }, ["A", "B", "C"], ["G1", "_G2A", "_G2B"], ["_Y0", "_Y1", "_Y2", "_Y3", "_Y4", "_Y5", "_Y6", "_Y7"]);
 });
 $(document).on("pagecreate", function () {
     andsetup();
--- EECircuit1/EECircuitWeb2/Scripts/logic.ts
+++ EECircuit1/EECircuitWeb2/Scripts/logic.ts
@@ -106,22 +106,56 @@ function setupOutputFlags(outputLabels: string[]) {
 
 var thefunc;
 
-function setup(name: string, pictureName: string, func, inputLabels: string[], outputLabels: string[]) {
-    if (!inputLabels) inputLabels = ["A", "B"];
-    if (!outputLabels) outputLabels = ["Q"];
+class LogicTableUnit {
+    public outputValues: boolean[] = [];
+}
 
-    thefunc = func;
+class LogicTable {
+    public logicTables: LogicTableUnit[] = [];
+}
 
+function createLogicalTable(ignoreableInputLabels: string[], inputLabels: string[], outputLabels: string[]): LogicTable {
+    var table = new LogicTable();
+    for (var i = 0; i < Math.pow(2,ignoreableInputLabels.length); i++) {
+        var unit = new LogicTableUnit();
+        var inputValues: boolean[] = [];
+        var t = i;
+        for (var j = 0; j < inputLabels.length; j++) {
+            inputValues.push(((t & 1) != 0));
+            t = t >> 1;
+        }
+        for (var j = 0; j < Math.pow(2, inputLabels.length); j++) {
+            unit.outputValues.push(thefunc(inputValues));
+        }
+        table.logicTables.push(unit);
+    }
+    return table;
+}
+function optimizeTable(table: LogicTable)
+{
+
+}
+
+function createLabel(label:string)
+{
+    var thi = document.createElement("th");
+    if (label.substring(0, 1) == "_") {
+        var span = document.createElement("span");
+        $(span).css("text-decoration", "overline");
+        $(span).text(label.substring(1));
+        $(thi).append(span);
+    }
+    else {
+        $(thi).text(label);
+    }
+    $(thi).addClass("borderh");
+    return thi;
+}
+
+function createHtmlTable(logicTable: LogicTable, ignoreableInputLabels: string[], inputLabels: string[], outputLabels: string[])
+{
     var expand = false;
     if (inputLabels.length + outputLabels.length <= 3) expand = true;
-
-    $("#logicname").text(name);
-    $("#logicicon").attr("src", "/Content/images/gate/" + pictureName + ".png");
-    $("#simname").text(name + "・シミュレーター");
-    $("#tablename").text(name + "真理表");
-
-    setupInputChecks(inputLabels);
-    setupOutputFlags(outputLabels);
 
     $("#tableroot").empty();
     var table = document.createElement("table");
@@ -150,41 +184,40 @@ function setup(name: string, pictureName: string, func, inputLabels: string[], o
     $(tdInput).append(tableInput);
     var trih = document.createElement("tr");
     $(tableInput).append(trih);
+    for (var i = 0; i < ignoreableInputLabels.length; i++) {
+        $(trih).append(createLabel(ignoreableInputLabels[i]));
+    }
     for (var i = 0; i < inputLabels.length; i++) {
-        var thi = document.createElement("th");
-        if (inputLabels[i].substring(0, 1) == "_")
-        {
-            var span = document.createElement("span");
-            $(span).css("text-decoration", "overline");
-            $(span).text(inputLabels[i].substring(1));
-            $(thi).append(span);
-        }
-        else
-        {
-            $(thi).text(inputLabels[i]);
-        }
-        $(thi).addClass("borderh");
-        $(trih).append(thi);
+        $(trih).append(createLabel(inputLabels[i]));
     }
 
-    for (var j = 0; j < totalCount; j++) {
-        var trid = document.createElement("tr");
-        $(trid).addClass("tr" + j);
-        $(trid).addClass("trall");
-        $(tableInput).append(trid);
-        var values: boolean[] = [];
-        var t = j;
-        for (var i = 0; i < inputLabels.length; i++) {
-            values.push(((t & 1) != 0));
-            t = t >> 1;
-        }
-        for (var i = 0; i < inputLabels.length; i++) {
-            var thd = document.createElement("td");
-            $(thd).addClass("border");
-            if (expand) $(thd).addClass("thick");
-            $(thd).addClass(values[i] ? "one" : "zero");
-            $(thd).text(values[i] ? "1" : "0");
-            $(trid).append(thd);
+    var count= 0;
+    for (var j = 0; j < logicTable.logicTables.length; j++) {
+        for (var k = 0; k < logicTable.logicTables[j].outputValues.length; k++) {
+            var trid = document.createElement("tr");
+            $(trid).addClass("tr" + count);
+            $(trid).addClass("trall");
+            $(tableInput).append(trid);
+            var values: boolean[] = [];
+            var t = k;
+            for (var i = 0; i < inputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            var t = j;
+            for (var i = 0; i < ignoreableInputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            for (var i = 0; i < values.length; i++) {
+                var thd = document.createElement("td");
+                $(thd).addClass("border");
+                if (expand) $(thd).addClass("thick");
+                $(thd).addClass(values[i] ? "one" : "zero");
+                $(thd).text(values[i] ? "1" : "0");
+                $(trid).append(thd);
+            }
+            count++;
         }
     }
 
@@ -209,28 +242,57 @@ function setup(name: string, pictureName: string, func, inputLabels: string[], o
         $(troh).append(tho);
     }
 
-    for (var j = 0; j < totalCount; j++) {
-        var trod = document.createElement("tr");
-        $(trod).addClass("tr" + j);
-        $(trod).addClass("trall");
-        $(tableOutput).append(trod);
-        var values: boolean[] = [];
-        var t = j;
-        for (var i = 0; i < inputLabels.length; i++) {
-            values.push(((t & 1) != 0));
-            t = t >> 1;
-        }
-        var results: boolean[] = thefunc(values);
-        for (var i = 0; i < outputLabels.length; i++) {
-            var thd = document.createElement("td");
-            $(thd).addClass("border");
-            if (expand) $(thd).addClass("thick");
-            $(thd).addClass("result");
-            $(thd).addClass(results[i] ? "one" : "zero");
-            $(thd).text(results[i] ? "1" : "0");
-            $(trod).append(thd);
+    var count = 0;
+    for (var j = 0; j < logicTable.logicTables.length; j++) {
+        for (var k = 0; k < logicTable.logicTables[j].outputValues.length; k++) {
+            var trod = document.createElement("tr");
+            $(trod).addClass("tr" + count);
+            $(trod).addClass("trall");
+            $(tableOutput).append(trod);
+            var values: boolean[] = [];
+            var t = k;
+            for (var i = 0; i < inputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            var t = j;
+            for (var i = 0; i < ignoreableInputLabels.length; i++) {
+                values.push(((t & 1) != 0));
+                t = t >> 1;
+            }
+            var results: boolean[] = thefunc(values);
+            for (var i = 0; i < outputLabels.length; i++) {
+                var thd = document.createElement("td");
+                $(thd).addClass("border");
+                if (expand) $(thd).addClass("thick");
+                $(thd).addClass("result");
+                $(thd).addClass(results[i] ? "one" : "zero");
+                $(thd).text(results[i] ? "1" : "0");
+                $(trod).append(thd);
+            }
+            count++;
         }
     }
+}
+
+function setup(name: string, pictureName: string, func, ignoreableInputLabels: string[], inputLabels: string[], outputLabels: string[]) {
+    if (!ignoreableInputLabels) ignoreableInputLabels = [];
+    if (!inputLabels) inputLabels = ["A", "B"];
+    if (!outputLabels) outputLabels = ["Q"];
+
+    thefunc = func;
+
+    $("#logicname").text(name);
+    $("#logicicon").attr("src", "/Content/images/gate/" + pictureName + ".png");
+    $("#simname").text(name + "・シミュレーター");
+    $("#tablename").text(name + "真理表");
+
+    setupInputChecks(ignoreableInputLabels.concat(inputLabels));
+    setupOutputFlags(outputLabels);
+
+    var table = createLogicalTable(ignoreableInputLabels, inputLabels, outputLabels);
+    optimizeTable(table);
+    createHtmlTable(table, ignoreableInputLabels, inputLabels, outputLabels);
 
     update();
 }
@@ -238,18 +300,18 @@ function setup(name: string, pictureName: string, func, inputLabels: string[], o
 function andsetup() {
     setup("AND GATE", "AND", (input: boolean[]): boolean[] => {
         return [input[0] && input[1]];
-    }, null, null);
+    }, null, null, null);
 }
 
 $("#navnot").click(() => {
     setup("NOT GATE", "NOT", (input: boolean[]): boolean[] => {
         return [!input[0]];
-    }, ["A"], null);
+    }, null, ["A"], null);
 });
 $("#navor").click(() => {
     setup("OR GATE", "OR", (input: boolean[]): boolean[] => {
         return [input[0] || input[1]];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navand").click(() => {
     andsetup();
@@ -257,29 +319,29 @@ $("#navand").click(() => {
 $("#navand4").click(() => {
     setup("AND(4Input) GATE", "AND(4Input)", (input: boolean[]): boolean[] => {
         return [input[0] && input[1] && input[2] && input[3]];
-    }, ["A", "B", "C", "D"], null);
+    }, null, ["A", "B", "C", "D"], null);
 });
 $("#navxor").click(() => {
     setup("XOR GATE", "XOR", (input: boolean[]): boolean[] => {
         return [input[0] !== input[1]];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navnor").click(() => {
     setup("NOR GATE", "NOR", (input: boolean[]): boolean[] => {
         return [!(input[0] || input[1])];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navnand").click(() => {
     setup("NAND GATE", "NAND", (input: boolean[]): boolean[] => {
         return [!(input[0] && input[1])];
-    }, null, null);
+    }, null, null, null);
 });
 $("#navdec").click(() => {
     setup("DECODER", "DECODER", (input: boolean[]): boolean[] => {
         var n = -1;
         if (input[3] && !input[4] && !input[5]) n = (input[0] ? 1 : 0) + (input[1] ? 2 : 0) + (input[2] ? 4 : 0)
         return [n != 0, n != 1, n != 2, n != 3, n != 4, n != 5, n != 6, n != 7];
-    }, ["A", "B", "C", "G1", "_G2A", "_G2B"], ["_Y0", "_Y1", "_Y2", "_Y3", "_Y4", "_Y5", "_Y6", "_Y7"]);
+    }, ["A", "B", "C"], ["G1", "_G2A", "_G2B"], ["_Y0", "_Y1", "_Y2", "_Y3", "_Y4", "_Y5", "_Y6", "_Y7"]);
 });
 
 $(document).on("pagecreate", function () {
