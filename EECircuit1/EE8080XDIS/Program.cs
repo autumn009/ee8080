f7d7c7352b43b85f1035d1740cae4e8a64ee9d3f
Kawamata Akira AK <autumn@piedey.co.jp>
2017-05-15 21:09:48 +0900

    temp save

--- EECircuit1/EE8080XDIS/Program.cs
+++ EECircuit1/EE8080XDIS/Program.cs
@@ -6,7 +6,18 @@ namespace EE8080XDIS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if( args.Length != 2)
+            {
+                Console.WriteLine("usage: ee8080xdis fullpath startaddressInHex");
+                Console.WriteLine("example: ee8080xdis c:\\m80.com 100");
+                return;
+            }
+            string path = args[0];
+            int offset = int.Parse(args[1], System.Globalization.NumberStyles.HexNumber);
+            byte[] image = System.IO.File.ReadAllBytes(path);
+            var dis = new Dis8080(image, offset);
+            dis.Diassemble();
+            Console.WriteLine("Done");
         }
     }
 }
---
+++ EECircuit1/EE8080XDIS/dis8080.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EE8080XDIS
+{
+    class Dis8080
+    {
+        private byte[] image;
+        private int offset;
+        private int pointer;
+        private int start;
+        private int length;
+        private byte [] bytes = new byte[3];
+        private string mnem;
+        private string arg1;
+        private string arg2;
+
+        private byte getNextByte()
+        {
+            if (pointer >= image.Length) return 0;
+            return image[pointer++];
+        }
+
+        private void disasseleLine()
+        {
+            start = pointer;
+            arg1 = null;
+            arg2 = null;
+            length = 1;
+            bytes[0] = getNextByte();
+
+            mnem = "NOP";
+        }
+        private void output()
+        {
+            Console.Write("{0,0:X4} ", start);
+            for (int i = 0; i < 3; i++)
+            {
+                if (i < length) Console.Write("{0,0:X2} ", bytes[i]);
+                else Console.Write("   ");
+            }
+            Console.Write("{0} ", mnem);
+            if (arg1 != null) Console.Write("{0}", arg1);
+            if (arg2 != null) Console.Write(",{0}", arg2);
+            Console.WriteLine();
+        }
+        public void Diassemble()
+        {
+            pointer = 0;
+            while(pointer < image.Length)
+            {
+                disasseleLine();
+                output();
+            }
+        }
+
+        public Dis8080(byte[] image, int offset )
+        {
+            this.image = image;
+            this.offset = offset;
+        }
+    }
+}
